3a4ce358d3b2edcd2183b73af424139be19bec9c
Mathieu Lala <m.lala@viridit.com>
2021-11-29 21:39:16 +0100

    test: add protocol test & license

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,19 @@
+/**
+ * vSMTP mail transfer agent
+ * Copyright (C) 2021 viridIT SAS
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see https://www.gnu.org/licenses/.
+ *
+**/
 use crate::mailprocessing::mail_receiver::MailReceiver;
 use crate::resolver::DataEndResolver;
 
@@ -195,7 +211,7 @@ where
         tokio::spawn(async move {
             log::warn!("Handling client: {}", client_addr);
 
-            match MailReceiver::<R>::new(&client_addr, tls_config, tls_security_level)
+            match MailReceiver::<R>::new(client_addr, tls_config, tls_security_level)
                 .receive_plain(&mut stream)
                 .await
             {
