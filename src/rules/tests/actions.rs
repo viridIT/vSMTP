e231c2c2745be0baf6ba42f412328e9e8c9bf844
ltabis <tab.rysak70@gmail.com>
2021-12-07 10:27:18 +0100

    :white_check_mark: added a macro to easly generate rule matching tests.

--- src/rules/tests/actions.rs
+++ src/rules/tests/actions.rs
@@ -1,32 +1,45 @@
+#[allow(unused)]
+macro_rules! generate_rule_check_test {
+    ($init:expr, connect, $($against:expr, $should_be:ident),*) => {
+        let obj = $init();
+        $(
+            println!("object {:?} {} connect {:?}", obj, if $should_be == true { "should be" } else {"should not be"}, $against);
+            assert_eq!(
+                internal_is_connect(&$against, &obj),
+                $should_be
+            );
+        )*
+    };
+}
+
+// TODO: generate those tests using a macro.
 #[cfg(test)]
 mod test {
     use crate::rules::actions::*;
     use crate::rules::obj::*;
-    use ipnet::*;
     use rhai::Map;
     use std::net::*;
-    use std::str::FromStr;
 
     #[test]
     fn test_connect() {
         // ip4 / ip6
         {
-            assert!(internal_is_connect(
-                &IpAddr::V4(Ipv4Addr::UNSPECIFIED),
-                &Object::Ip4(Ipv4Addr::UNSPECIFIED),
-            ),);
-            assert!(!internal_is_connect(
-                &IpAddr::V4(Ipv4Addr::LOCALHOST),
-                &Object::Ip4(Ipv4Addr::UNSPECIFIED),
-            ),);
-            assert!(internal_is_connect(
-                &IpAddr::V6(Ipv6Addr::UNSPECIFIED),
-                &Object::Ip6(Ipv6Addr::UNSPECIFIED),
-            ),);
-            assert!(!internal_is_connect(
-                &IpAddr::V6(Ipv6Addr::LOCALHOST),
-                &Object::Ip6(Ipv6Addr::UNSPECIFIED),
-            ),);
+            generate_rule_check_test!(
+                || Object::Ip4(Ipv4Addr::UNSPECIFIED),
+                connect,
+                IpAddr::V4(Ipv4Addr::UNSPECIFIED),
+                true,
+                IpAddr::V4(Ipv4Addr::LOCALHOST),
+                false
+            );
+            generate_rule_check_test!(
+                || Object::Ip6(Ipv6Addr::UNSPECIFIED),
+                connect,
+                IpAddr::V6(Ipv6Addr::UNSPECIFIED),
+                true,
+                IpAddr::V6(Ipv6Addr::LOCALHOST),
+                false
+            );
         }
 
         // TODO: test ranges.
@@ -39,58 +52,104 @@ mod test {
 
         // regex.
         {
-            assert!(internal_is_connect(
-                &IpAddr::V4(Ipv4Addr::UNSPECIFIED),
-                &Object::Regex("^[a-z0-9.]+0.0$".parse().unwrap()),
-            ),);
-            assert!(internal_is_connect(
-                &IpAddr::V4(Ipv4Addr::from_str("127.1.0.0").unwrap()),
-                &Object::Regex("^[a-z0-9.]+0.0$".parse().unwrap()),
-            ),);
-            assert!(!internal_is_connect(
-                &IpAddr::V4(Ipv4Addr::from_str("127.0.0.1").unwrap()),
-                &Object::Regex("^[a-z0-9.]+0.0$".parse().unwrap()),
-            ),);
+            generate_rule_check_test!(
+                || Object::Regex("^[a-z0-9.]+0.0$".parse().unwrap()),
+                connect,
+                IpAddr::V4(Ipv4Addr::UNSPECIFIED),
+                true,
+                IpAddr::V4(Ipv4Addr::LOCALHOST),
+                false,
+                IpAddr::V4(Ipv4Addr::new(127, 90, 0, 0)),
+                true
+            );
         }
 
         // files & group.
         {
-            let mut file = Map::new();
-            file.insert("type".into(), "file".into());
-            file.insert("content_type".into(), "addr".into());
-            file.insert(
-                "value".into(),
-                "src/rules/tests/configs/whitelist.txt".into(),
+            generate_rule_check_test!(
+                || {
+                    let mut file = Map::new();
+                    file.insert("type".into(), "file".into());
+                    file.insert("content_type".into(), "addr".into());
+                    file.insert(
+                        "value".into(),
+                        "src/rules/tests/configs/whitelist.txt".into(),
+                    );
+
+                    Object::from(&file).unwrap()
+                },
+                connect,
+                // the whitelist doesn't contain ips, so everything is false.
+                IpAddr::V4(Ipv4Addr::UNSPECIFIED),
+                false,
+                IpAddr::V4(Ipv4Addr::LOCALHOST),
+                false,
+                IpAddr::V4(Ipv4Addr::new(127, 90, 0, 0)),
+                false
             );
 
-            let file = Object::from(&file).unwrap();
+            generate_rule_check_test!(
+                || {
+                    let mut file = Map::new();
+                    file.insert("type".into(), "file".into());
+                    file.insert("content_type".into(), "ip4".into());
+                    file.insert("value".into(), "src/rules/tests/configs/hosts.txt".into());
 
-            assert!(!internal_is_connect(
-                &IpAddr::V4(Ipv4Addr::UNSPECIFIED),
-                &file,
-            ),);
+                    Object::from(&file).unwrap()
+                },
+                connect,
+                IpAddr::V4(Ipv4Addr::UNSPECIFIED),
+                false,
+                IpAddr::V4(Ipv4Addr::LOCALHOST),
+                true,
+                IpAddr::V4(Ipv4Addr::new(192, 168, 1, 91)),
+                true,
+                IpAddr::V4(Ipv4Addr::new(192, 168, 1, 93)),
+                true,
+                IpAddr::V4(Ipv4Addr::new(192, 168, 1, 94)),
+                false
+            );
 
-            let group = Object::Group(vec![
-                Object::Address("jones@foo.com".to_string()),
-                Object::Ip4("0.0.0.0".parse().unwrap()),
-            ]);
-
-            assert!(internal_is_connect(
-                &IpAddr::V4(Ipv4Addr::UNSPECIFIED),
-                &group,
-            ),);
+            generate_rule_check_test!(
+                || {
+                    Object::Group(vec![
+                        Object::Address("jones@foo.com".to_string()),
+                        Object::Ip4("0.0.0.0".parse().unwrap()),
+                    ])
+                },
+                connect,
+                IpAddr::V4(Ipv4Addr::UNSPECIFIED),
+                true,
+                IpAddr::V4(Ipv4Addr::LOCALHOST),
+                false,
+                IpAddr::V4(Ipv4Addr::new(192, 168, 1, 91)),
+                false
+            );
         }
 
         // invalid.
         {
-            assert!(!internal_is_connect(
-                &IpAddr::V4(Ipv4Addr::UNSPECIFIED),
-                &Object::Var("".to_string())
-            ),);
-            assert!(!internal_is_connect(
-                &IpAddr::V4(Ipv4Addr::UNSPECIFIED),
-                &Object::Fqdn("foo.com".to_string())
-            ),);
+            generate_rule_check_test!(
+                || Object::Var("".to_string()),
+                connect,
+                IpAddr::V4(Ipv4Addr::UNSPECIFIED),
+                false,
+                IpAddr::V4(Ipv4Addr::LOCALHOST),
+                false,
+                IpAddr::V4(Ipv4Addr::new(192, 168, 1, 91)),
+                false
+            );
+
+            generate_rule_check_test!(
+                || Object::Fqdn("foo.com".to_string()),
+                connect,
+                IpAddr::V4(Ipv4Addr::UNSPECIFIED),
+                false,
+                IpAddr::V4(Ipv4Addr::LOCALHOST),
+                false,
+                IpAddr::V4(Ipv4Addr::new(192, 168, 1, 91)),
+                false
+            );
         }
     }
 }
