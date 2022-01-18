c811e4a18212984c110851921abe831f36bd8e83
ltabis <tab.rysak70@gmail.com>
2022-01-18 16:11:43 +0100

    :white_check_mark: added headers to properly test utf-8 in email body.

--- tests/integration/protocol/utf8.rs
+++ tests/integration/protocol/utf8.rs
@@ -3,9 +3,8 @@ mod tests {
     use crate::integration::protocol::get_test_config;
     use vsmtp::test_helpers::test_receiver;
     use vsmtp::{
-        config::server_config::ServerConfig, mime::mail::BodyType, model::mail::Body,
-        model::mail::MailContext, resolver::DataEndResolver, rules::address::Address,
-        smtp::code::SMTPReplyCode,
+        config::server_config::ServerConfig, model::mail::Body, model::mail::MailContext,
+        resolver::DataEndResolver, rules::address::Address, smtp::code::SMTPReplyCode,
     };
 
     macro_rules! test_lang {
@@ -26,7 +25,10 @@ mod tests {
                         std::collections::HashSet::from([Address::new("aa@bb").unwrap()])
                     );
                     assert!(match &ctx.body {
-                        Body::Parsed(mail) => mail.body == BodyType::Undefined,
+                        Body::Parsed(mail) => {
+                            let (headers, body) = mail.to_raw();
+                            format!("{headers}\n\n{body}\n").as_str() == include_str!($lang_code)
+                        }
                         _ => false,
                     });
 
