aea7e8afea118823f8f0dcbf67157ac5ced7bee5
Mathieu Lala <m.lala@viridit.com>
2022-02-03 17:54:07 +0100

    fix: fuzzer

--- fuzz/fuzz_targets/receiver.rs
+++ fuzz/fuzz_targets/receiver.rs
@@ -4,24 +4,24 @@ use vsmtp::{
     config::server_config::ServerConfig,
     connection::{Connection, Kind},
     io_service::IoService,
+    processes::ProcessMessage,
     server::ServerVSMTP,
-    test_helpers::{DefaultResolverTest, Mock},
+    test_helpers::Mock,
 };
 
 fuzz_target!(|data: &[u8]| {
-    let mut config = std::sync::Arc::new(
-        ServerConfig::builder()
-            .with_server_default_port("fuzz.server.com")
-            .without_log()
-            .without_smtps()
-            .with_default_smtp()
-            .with_delivery("./tmp/fuzz/", vsmtp::collection! {})
-            .with_rules("./tmp/no_rules")
-            .with_default_reply_codes()
-            .build(),
-    );
-
+    let mut config = ServerConfig::builder()
+        .with_server_default_port("fuzz.server.com")
+        .without_log()
+        .without_smtps()
+        .with_default_smtp()
+        .with_delivery("./tmp/fuzz/", vsmtp::collection! {})
+        .with_rules("./tmp/no_rules")
+        .with_default_reply_codes()
+        .build();
     config.smtp.error.soft_count = -1;
+
+    let config = std::sync::Arc::new(config);
 
     let mut written_data = Vec::new();
     let mut mock = Mock::new(data.to_vec(), &mut written_data);
@@ -34,16 +34,17 @@ fuzz_target!(|data: &[u8]| {
     )
     .unwrap();
 
+    let (working_sender, _) = tokio::sync::mpsc::channel::<ProcessMessage>(1);
+    let (delivery_sender, _) = tokio::sync::mpsc::channel::<ProcessMessage>(1);
+
     let _ = match tokio::runtime::Runtime::new() {
         Ok(r) => r,
         Err(_) => todo!(),
     }
-    .block_on(ServerVSMTP::handle_connection::<
-        DefaultResolverTest,
-        Mock<'_>,
-    >(
+    .block_on(ServerVSMTP::handle_connection(
         &mut conn,
-        std::sync::Arc::new(tokio::sync::Mutex::new(DefaultResolverTest)),
+        std::sync::Arc::new(working_sender),
+        std::sync::Arc::new(delivery_sender),
         None,
     ));
 });
