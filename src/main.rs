f7537ff2a6ddc5bb060e8c4bfb40a7f4ca1eb209
Mathieu Lala <m.lala@viridit.com>
2022-02-12 19:40:02 +0100

    feat: add thread count for server in config

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,27 @@ use vsmtp::resolver::smtp_resolver::SMTPResolver;
 use vsmtp::rules::rule_engine;
 use vsmtp::server::ServerVSMTP;
 
+async fn server_main(config: std::sync::Arc<ServerConfig>) -> anyhow::Result<()> {
+    log4rs::init_config(get_logger_config(&config)?)?;
+
+    rule_engine::init(Box::leak(config.rules.dir.clone().into_boxed_str())).map_err(|error| {
+        log::error!("could not initialize the rule engine: {}", error);
+        error
+    })?;
+
+    let mut server = ServerVSMTP::new(config)
+        .await
+        .expect("Failed to create the server");
+    log::warn!("Listening on: {:?}", server.addr());
+
+    server
+        .with_resolver("maildir", MailDirResolver::default())
+        .with_resolver("smtp", SMTPResolver::default())
+        .with_resolver("mbox", MBoxResolver::default())
+        .listen_and_serve()
+        .await
+}
+
 #[derive(clap::Parser, Debug)]
 #[clap(about, version, author)]
 #[clap(global_setting(clap::AppSettings::UseLongFormatForHelpSubcommand))]
@@ -41,65 +62,50 @@ enum Commands {
     ConfigDiff,
 }
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
+fn main() -> anyhow::Result<()> {
     let args = <Args as clap::StructOpt>::parse();
     println!("Loading configuration at path='{}'", args.config);
 
-    let config = std::sync::Arc::new(ServerConfig::from_toml(
-        &std::fs::read_to_string(&args.config).expect("cannot read file"),
-    )?);
+    let config = ServerConfig::from_toml(&std::fs::read_to_string(&args.config)?)?;
 
-    match args.command {
-        Some(Commands::ConfigShow) => {
-            let stringified = serde_json::to_string_pretty(&config.as_ref())?;
-            println!("Loaded configuration: {}", stringified);
-            return Ok(());
-        }
-        Some(Commands::ConfigDiff) => {
-            let loaded_config = serde_json::to_string_pretty(&config.as_ref())?;
-            let default_config = serde_json::to_string_pretty(
-                &ServerConfig::builder()
-                    .with_rfc_port(&config.server.domain)
-                    .without_log()
-                    // TODO: default
-                    .without_smtps()
-                    .with_default_smtp()
-                    // TODO: default
-                    .with_delivery("/var/spool/vsmtp", vsmtp::collection! {})
-                    // TODO: default
-                    .with_rules("/etc/vsmtp/rules")
-                    .with_default_reply_codes()
-                    .build(),
-            )?;
-            for diff in diff::lines(&default_config, &loaded_config) {
-                match diff {
-                    diff::Result::Left(left) => println!("-\x1b[0;31m{left}\x1b[0m"),
-                    diff::Result::Both(same, _) => println!(" {same}"),
-                    diff::Result::Right(right) => println!("+\x1b[0;32m{right}\x1b[0m"),
-                }
+    if let Some(command) = args.command {
+        match command {
+            Commands::ConfigShow => {
+                let stringified = serde_json::to_string_pretty(&config)?;
+                println!("Loaded configuration: {}", stringified);
+                return Ok(());
             }
-            return Ok(());
+            Commands::ConfigDiff => {
+                let loaded_config = serde_json::to_string_pretty(&config)?;
+                let default_config = serde_json::to_string_pretty(
+                    &ServerConfig::builder()
+                        .with_rfc_port(&config.server.domain, None)
+                        .without_log()
+                        // TODO: default
+                        .without_smtps()
+                        .with_default_smtp()
+                        // TODO: default
+                        .with_delivery("/var/spool/vsmtp", vsmtp::collection! {})
+                        // TODO: default
+                        .with_rules("/etc/vsmtp/rules")
+                        .with_default_reply_codes()
+                        .build(),
+                )?;
+                for diff in diff::lines(&default_config, &loaded_config) {
+                    match diff {
+                        diff::Result::Left(left) => println!("-\x1b[0;31m{left}\x1b[0m"),
+                        diff::Result::Both(same, _) => println!(" {same}"),
+                        diff::Result::Right(right) => println!("+\x1b[0;32m{right}\x1b[0m"),
+                    }
+                }
+                return Ok(());
+            }
         }
-        _ => (),
-    };
+    }
 
-    log4rs::init_config(get_logger_config(&config)?)?;
-
-    rule_engine::init(Box::leak(config.rules.dir.clone().into_boxed_str())).map_err(|error| {
-        log::error!("could not initialize the rule engine: {}", error);
-        error
-    })?;
-
-    let mut server = ServerVSMTP::new(config)
-        .await
-        .expect("Failed to create the server");
-    log::warn!("Listening on: {:?}", server.addr());
-
-    server
-        .with_resolver("maildir", MailDirResolver::default())
-        .with_resolver("smtp", SMTPResolver::default())
-        .with_resolver("mbox", MBoxResolver::default())
-        .listen_and_serve()
-        .await
+    tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(config.server.thread_count)
+        .enable_all()
+        .build()?
+        .block_on(server_main(std::sync::Arc::new(config)))
 }
