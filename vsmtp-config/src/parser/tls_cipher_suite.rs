a78a9e70e11fc058d49974544e7e79273cff3575
Mathieu Lala <m.lala@viridit.com>
2022-04-01 01:17:40 +0200

    test: add test on tls cipher suite and user/group serialisation

--- vsmtp-config/src/parser/tls_cipher_suite.rs
+++ vsmtp-config/src/parser/tls_cipher_suite.rs
@@ -24,7 +24,7 @@ impl std::str::FromStr for CipherSuite {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
             "TLS_AES_256_GCM_SHA384" => Ok(Self(rustls::CipherSuite::TLS13_AES_256_GCM_SHA384)),
-            "TLS_AES_128_GCM_SHA256" => Ok(Self(rustls::CipherSuite::TLS13_AES_128_CCM_8_SHA256)),
+            "TLS_AES_128_GCM_SHA256" => Ok(Self(rustls::CipherSuite::TLS13_AES_128_GCM_SHA256)),
             "TLS_CHACHA20_POLY1305_SHA256" => {
                 Ok(Self(rustls::CipherSuite::TLS13_CHACHA20_POLY1305_SHA256))
             }
@@ -147,4 +147,93 @@ where
         serde::ser::SerializeSeq::serialize_element(&mut seq, &CipherSuite(*i))?;
     }
     serde::ser::SerializeSeq::end(seq)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parser::tls_cipher_suite::CipherSuite;
+
+    #[derive(serde::Serialize, serde::Deserialize)]
+    struct S {
+        #[serde(
+            serialize_with = "crate::parser::tls_cipher_suite::serialize",
+            deserialize_with = "crate::parser::tls_cipher_suite::deserialize"
+        )]
+        v: Vec<rustls::CipherSuite>,
+    }
+
+    #[test]
+    fn error() {
+        assert!(toml::from_str::<S>(r#"v = ["SRP_SHA_WITH_AES_128_CBC_SHA"]"#).is_err());
+        assert!(toml::from_str::<S>(r#"v = "foobar""#).is_err());
+        assert!(toml::from_str::<S>(r#"v = 100"#).is_err());
+    }
+
+    #[test]
+    fn tls1_3() {
+        assert_eq!(
+            toml::from_str::<S>(
+                r#"v = [
+    "TLS_AES_256_GCM_SHA384",
+    "TLS_AES_128_GCM_SHA256",
+    "TLS_CHACHA20_POLY1305_SHA256"
+]"#
+            )
+            .unwrap()
+            .v,
+            vec![
+                rustls::CipherSuite::TLS13_AES_256_GCM_SHA384,
+                rustls::CipherSuite::TLS13_AES_128_GCM_SHA256,
+                rustls::CipherSuite::TLS13_CHACHA20_POLY1305_SHA256,
+            ]
+        );
+    }
+
+    #[test]
+    fn tls1_2() {
+        assert_eq!(
+            toml::from_str::<S>(
+                r#"v = [
+    "ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
+    "ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
+    "ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256",
+    "ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+    "ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+    "ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256",
+]"#
+            )
+            .unwrap()
+            .v,
+            vec![
+                rustls::CipherSuite::TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+                rustls::CipherSuite::TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+                rustls::CipherSuite::TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+                rustls::CipherSuite::TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+                rustls::CipherSuite::TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+                rustls::CipherSuite::TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+            ]
+        );
+    }
+
+    const ALL_CIPHER_SUITE: [rustls::CipherSuite; 9] = [
+        rustls::CipherSuite::TLS13_AES_256_GCM_SHA384,
+        rustls::CipherSuite::TLS13_AES_128_GCM_SHA256,
+        rustls::CipherSuite::TLS13_CHACHA20_POLY1305_SHA256,
+        rustls::CipherSuite::TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+        rustls::CipherSuite::TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+        rustls::CipherSuite::TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+        rustls::CipherSuite::TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+        rustls::CipherSuite::TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+        rustls::CipherSuite::TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+    ];
+
+    #[test]
+    fn serialize() {
+        for i in ALL_CIPHER_SUITE {
+            assert_eq!(
+                serde_json::to_string(&S { v: vec![i] }).unwrap(),
+                format!("{{\"v\":[\"{}\"]}}", CipherSuite(i))
+            );
+        }
+    }
 }
