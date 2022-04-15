24bacce792f622d337072f88b3d0a579d67d635c
ltabis <ltabis.pro@gmail.com>
2022-04-15 16:56:54 +0200

    :white_check_mark: add tests for toml config in rule engine.

--- vsmtp-rule-engine/src/tests/types/mod.rs
+++ vsmtp-rule-engine/src/tests/types/mod.rs
@@ -90,12 +90,6 @@ fn test_objects() {
 
 #[test]
 fn test_services() {
-    let re = RuleEngine::new(
-        &vsmtp_config::Config::default(),
-        &Some(rules_path!["service", "main.vsl"]),
-    )
-    .unwrap();
-
     let config = Config::builder()
         .with_version_str("<1.0.0")
         .unwrap()
@@ -124,9 +118,49 @@ fn test_services() {
         .validate()
         .unwrap();
 
+    let re = RuleEngine::new(&config, &Some(rules_path!["service", "main.vsl"])).unwrap();
+
     let mut state = RuleState::new(&config);
 
     state.get_context().write().unwrap().body = Body::Raw(String::default());
 
     assert_eq!(re.run_when(&mut state, &StateSMTP::Connect), Status::Accept);
+}
+
+#[test]
+fn test_config_display() {
+    let config = Config::builder()
+        .with_version_str("<1.0.0")
+        .unwrap()
+        .with_server_name("testserver.com")
+        .with_user_group_and_default_system("root", "root")
+        .unwrap()
+        .with_ipv4_localhost()
+        .with_default_logs_settings()
+        .with_spool_dir_and_default_queues("./tmp/delivery")
+        .without_tls_support()
+        .with_default_smtp_options()
+        .with_default_smtp_error_handler()
+        .with_default_smtp_codes()
+        .without_auth()
+        .with_app_at_location("./tmp/app")
+        .with_vsl("./tmp/nothing")
+        .with_default_app_logs()
+        .with_services(collection! {"shell".to_string() => Service::UnixShell {
+            timeout: std::time::Duration::from_secs(2),
+            user: None,
+            group: None,
+            command: "echo".to_string(),
+            args: Some("test".to_string()),
+        }})
+        .with_system_dns()
+        .validate()
+        .unwrap();
+
+    let re = RuleEngine::new(&config, &Some(rules_path!["objects", "main.vsl"])).unwrap();
+    let mut state = RuleState::new(&config);
+
+    state.get_context().write().unwrap().body = Body::Raw(String::default());
+
+    assert_eq!(re.run_when(&mut state, &StateSMTP::Helo), Status::Accept);
 }
